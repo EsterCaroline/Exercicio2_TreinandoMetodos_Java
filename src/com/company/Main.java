b9672c0f57e6d4bf7e709029c4d5094303f6e653
Ester <ester.caroline.1009@gmail.com>
2022-04-29 16:32:33 -0300

    versao 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Quadrilatero
+        System.out.println("Exercicio quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+    }
+}
---
+++ src/com/company/Quadrilatero.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Quadrilatero {
+    public static void area (double lado){
+
+        System.out.println("A area do quadrado :"+ lado*lado );
+    }
+    public static void area (double lado1,double lado2){
+
+        System.out.println("A area do retangulo :"+ lado1 * lado2);
+    }
+    public static void area (double baseMaior,double baseMenor , double altura){
+
+        System.out.println("A area do trapézio :"+ ((baseMaior+baseMenor)*altura) / 2);
+    }
+    public static void area (float diagonal1,float diagonal2){
+
+        System.out.println("A area do losango :"+ (diagonal1 * diagonal2)/2);
+    }
+
+}
